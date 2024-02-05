07b5fd90a5a8fa318a48fb931bbbbfe434b684be
Nikolay Voronin <125467426+wnn44@users.noreply.github.com>
2024-02-05 22:59:30 +0300

    Враги преследуют игрока

--- Assets/Scripts/EnemyLookingTarget.cs
+++ Assets/Scripts/EnemyLookingTarget.cs
@@ -1,22 +1,33 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyLookingTarget : MonoBehaviour
 {
-    [SerializeField] private LayerMask _platformLayer;
+    [SerializeField] private PlayerType _player;
 
     private void Update()
     {
-        Debug.Log(CheckPlayerVisibil());
+        CheckPlayerVisibil();
+
     }
 
     private bool CheckPlayerVisibil()
     {
-      
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector3.left, 10, _platformLayer);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position + Vector3.up * 0.5f, _player.transform.position + Vector3.up * 0.5f - transform.position);
 
-        Debug.DrawRay(transform.position, Vector3.left,Color.red, 10);
+        Debug.DrawRay(transform.position + Vector3.up * 0.5f, _player.transform.position + Vector3.up * 0.5f - transform.position, Color.red);
+
+        if (hit.collider != null)
+        {
+            if (hit.collider.gameObject != _player.gameObject)
+            {
+                Debug.Log("Путь к игроку преграждает объект: " + hit.collider.name);
+            }
+            else
+            {
+                Debug.Log("Вижу игрока!!!");
+            }
+        }
 
         return hit.collider != null;
     }
--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -5,8 +5,10 @@ public class EnemyMove : MonoBehaviour
     [SerializeField] private float _speed;
     [SerializeField] private LayerMask _platformLayer;
 
+    [SerializeField] private PlayerType _player;
+
     private Vector3 _direction;
-    private bool _isGrounded;
+    private bool _isEndOfRoad;
     private SpriteRenderer _enemySprite;
 
     private void Start()
@@ -17,12 +19,13 @@ public class EnemyMove : MonoBehaviour
 
     private void Update()
     {
-        MoveEnemy();
+        // MoveEnemy();
+        Move();
     }
 
     private void MoveEnemy()
     {
-        if (!CheckGround())
+        if (!CheckingEndOfPath())
         {
             _direction *= -1.0f;
             _enemySprite.flipX = _direction.x < 0;
@@ -30,15 +33,49 @@ public class EnemyMove : MonoBehaviour
         transform.position = Vector3.MoveTowards(transform.position, transform.position + _direction, _speed * Time.deltaTime);
     }
 
-    private bool CheckGround()
+    private bool CheckingEndOfPath()
     {
-        float offset = 0.4f;
+        float offset = 0.3f;
 
-        RaycastHit2D hit = Physics2D.Raycast(transform.position + _direction * offset, Vector3.down, 1, _platformLayer);
-        RaycastHit2D hit1 = Physics2D.Raycast(transform.position + _direction, Vector3.left, 0, _platformLayer);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position + _direction * offset, Vector3.down, 1f, _platformLayer);
+        RaycastHit2D hit1 = Physics2D.Raycast(transform.position + _direction, Vector3.left, 0f, _platformLayer);
 
-        _isGrounded = (hit.collider != null && hit1.collider == null);
+        _isEndOfRoad = (hit.collider != null && hit1.collider == null);
 
-        return _isGrounded;
+        return _isEndOfRoad;
+    }
+
+    private void Move()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position + Vector3.up * 0.5f, _player.transform.position + Vector3.up * 0.5f - transform.position);
+
+        //Debug.DrawRay(transform.position + Vector3.up * 0.5f, _player.transform.position + Vector3.up * 0.5f - transform.position, Color.red);
+
+        if (!CheckingEndOfPath())
+        {
+            ChangeDirection();
+        }
+
+        if (hit.collider != null)
+        {
+            if (hit.collider.gameObject != _player.gameObject)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, transform.position + _direction, _speed * Time.deltaTime);
+            }
+            else
+            {
+                if (transform.position.x < (_player.transform.position + _direction).x && _enemySprite.flipX) ChangeDirection();
+
+                if (transform.position.x > (_player.transform.position + _direction).x && !_enemySprite.flipX) ChangeDirection();
+
+                transform.position = Vector3.MoveTowards(transform.position, _player.transform.position + _direction, _speed * Time.deltaTime);
+            }
+        }
+    }
+
+    private void ChangeDirection()
+    {
+        _direction *= -1.0f;
+        _enemySprite.flipX = _direction.x < 0;
     }
 }
