efdce2f305435f6324e35e607e445d76fc1434b9
Nikolay Voronin <125467426+wnn44@users.noreply.github.com>
2024-02-06 23:14:35 +0300

    Player detection

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -4,7 +4,6 @@ public class EnemyMove : MonoBehaviour
 {
     [SerializeField] private float _speed;
     [SerializeField] private LayerMask _platformLayer;
-
     [SerializeField] private PlayerType _player;
 
     private Vector3 _direction;
@@ -19,18 +18,9 @@ public class EnemyMove : MonoBehaviour
 
     private void Update()
     {
-        // MoveEnemy();
         Move();
-    }
 
-    private void MoveEnemy()
-    {
-        if (!CheckingEndOfPath())
-        {
-            _direction *= -1.0f;
-            _enemySprite.flipX = _direction.x < 0;
-        }
-        transform.position = Vector3.MoveTowards(transform.position, transform.position + _direction, _speed * Time.deltaTime);
+        DetectionPlayer();
     }
 
     private bool CheckingEndOfPath()
@@ -47,35 +37,29 @@ public class EnemyMove : MonoBehaviour
 
     private void Move()
     {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position + Vector3.up * 0.5f, _player.transform.position + Vector3.up * 0.5f - transform.position);
-
-        //Debug.DrawRay(transform.position + Vector3.up * 0.5f, _player.transform.position + Vector3.up * 0.5f - transform.position, Color.red);
-
         if (!CheckingEndOfPath())
         {
             ChangeDirection();
         }
 
-        if (hit.collider != null)
-        {
-            if (hit.collider.gameObject != _player.gameObject)
-            {
-                transform.position = Vector3.MoveTowards(transform.position, transform.position + _direction, _speed * Time.deltaTime);
-            }
-            else
-            {
-                if (transform.position.x < (_player.transform.position + _direction).x && _enemySprite.flipX) ChangeDirection();
-
-                if (transform.position.x > (_player.transform.position + _direction).x && !_enemySprite.flipX) ChangeDirection();
-
-                transform.position = Vector3.MoveTowards(transform.position, _player.transform.position + _direction, _speed * Time.deltaTime);
-            }
-        }
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + _direction, _speed * Time.deltaTime);
     }
 
     private void ChangeDirection()
     {
         _direction *= -1.0f;
         _enemySprite.flipX = _direction.x < 0;
+    }
+
+    private void DetectionPlayer()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position + Vector3.up * 0.5f, _player.transform.position + Vector3.up * 0.5f - transform.position);
+        Debug.DrawRay(transform.position + Vector3.up * 0.5f, _player.transform.position + Vector3.up * 0.5f - transform.position, Color.red);
+
+        if (hit.collider != null && hit.collider.gameObject == _player.gameObject)
+        {
+            if (transform.position.x < (_player.transform.position).x && _enemySprite.flipX) ChangeDirection();
+            if (transform.position.x > (_player.transform.position).x && !_enemySprite.flipX) ChangeDirection();
+        }
     }
 }
---
+++ Assets/Scripts/PlayerDetection.cs
@@ -0,0 +1,35 @@
+using System;
+using UnityEngine;
+
+public class PlayerDetection : MonoBehaviour
+{
+    [SerializeField] private PlayerType _player;
+
+    public static event Action<bool> PlayerDetected;
+
+    private SpriteRenderer _enemySprite;
+
+    private void Start()
+    {
+        _enemySprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+            PlayerDetected?.Invoke(DetectionPlayer());
+
+    }
+
+    private bool DetectionPlayer()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position + Vector3.up * 0.5f, _player.transform.position + Vector3.up * 0.5f - transform.position);
+        Debug.DrawRay(transform.position + Vector3.up * 0.5f, _player.transform.position + Vector3.up * 0.5f - transform.position, Color.red);
+
+        if (hit.collider != null && hit.collider.gameObject == _player.gameObject)
+        {
+            if (transform.position.x < (_player.transform.position).x && _enemySprite.flipX) return true;
+            if (transform.position.x > (_player.transform.position).x && !_enemySprite.flipX) return true;
+        }
+        return false;
+    }
+}
