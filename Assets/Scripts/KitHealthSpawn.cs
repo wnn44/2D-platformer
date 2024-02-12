59e816117ba2273844252f7d6a107d9685720350
Nikolay Voronin <125467426+wnn44@users.noreply.github.com>
2024-02-12 22:54:00 +0300

    мелкие правки

--- Assets/Scripts/KitHealthSpawn.cs
+++ Assets/Scripts/KitHealthSpawn.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class KitHealthSpawn : MonoBehaviour
 {
     [SerializeField] private List<GameObject> _spawnKitHealth = new List<GameObject>();
     [SerializeField] private KitHealthType _prefabKitHealth;
+
+    private int maxTimeSpawn = 20;
+    private int minTimeSpawn = 10;
 
     private void Start()
     {
@@ -25,17 +27,15 @@ public class KitHealthSpawn : MonoBehaviour
 
     public void Spawn()
     {
-
-        StartCoroutine(Spawn1());
-
-
+        StartCoroutine(CreateMedicines());
     }
 
-    IEnumerator Spawn1()
+    IEnumerator CreateMedicines()
     {
-        yield return new WaitForSeconds(10f);
-
         int numberSpawner = UnityEngine.Random.Range(0, _spawnKitHealth.Count);
+        int timeSpawn = UnityEngine.Random.Range(minTimeSpawn, maxTimeSpawn);        
+        
+        yield return new WaitForSeconds(timeSpawn);
 
         GameObject spawnPoint = _spawnKitHealth[numberSpawner];
         Vector2 positionSpawnPoint = spawnPoint.gameObject.transform.position;
