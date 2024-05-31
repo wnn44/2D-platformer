764b6b51b4c399ea0f63cba79c8e684480297c4d
Nikolay Voronin <125467426+wnn44@users.noreply.github.com>
2024-05-31 16:39:10 +0300

    PlayerSearch

--- Assets/Scripts/Enemy/PlayerSearch.cs
+++ Assets/Scripts/Enemy/PlayerSearch.cs
@@ -1,50 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
 
 public class PlayerSearch : MonoBehaviour
 {
     [SerializeField] private Player _player;
-    [SerializeField] private float _distance = 0.5f;
+    [SerializeField] private float _distance;
 
-    private void Start()
+    private EnemyMove _enemyMove;
+
+    private void Awake()
     {
         _player = GameObject.FindObjectOfType<Player>();
+        _enemyMove = GetComponent<EnemyMove>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         DetectionPlayer();
     }
 
     private void DetectionPlayer()
     {
-        float coordinateOffset = 0.5f;
+        Vector2 ray = transform.position;
+        Vector2 direction = transform.TransformDirection( _enemyMove.Direction);
 
-        Vector2 origin = transform.position;
-        Vector2 direction = _player.transform.position + Vector3.up * coordinateOffset - transform.position;
+        RaycastHit2D hit = Physics2D.Raycast(ray, direction, _distance);
 
-        RaycastHit2D hit = Physics2D.Raycast(origin, direction, _distance);
-
-        Debug.DrawLine(origin, _player.transform.position * _distance + Vector3.up * coordinateOffset, Color.red );
+        Debug.DrawRay(ray, direction * _distance, Color.red);
 
         if (hit.collider != null && hit.collider.gameObject == _player.gameObject)
         {
-            float angleRotationY = Mathf.Round(transform.rotation.eulerAngles.y);
-
-            if (transform.position.x < (_player.transform.position).x && angleRotationY == 0)
-            {
-                Debug.Log("1");
-                //ChangeDirection();
-            }
-
-            if (transform.position.x > (_player.transform.position).x && angleRotationY == 0)
-            {
-                Debug.Log("2");
-                //ChangeDirection();
-            }
+                Debug.Log(hit.collider.gameObject);
         }
     }
-
 }
